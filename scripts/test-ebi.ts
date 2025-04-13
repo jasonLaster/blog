f9120c1620238f260b87a6fccd9939583481d5b0
Jason Laster <jason.laster.11@gmail.com>
2025-04-13 16:20:47 -0700

    Send emails when the premium discount is too low

--- scripts/test-ebi.ts
+++ scripts/test-ebi.ts
@@ -1,5 +1,21 @@
-import { getEBIDetails } from "../app/lib/ebi";
+import { getEBIDetails, sendPremiumDiscountAlert } from "../app/lib/ebi";
 
-getEBIDetails().then((data) => {
-  console.log(data);
-});
+if (false) {
+  getEBIDetails().then((data) => {
+    console.log(data);
+  });
+}
+
+if (true) {
+  sendPremiumDiscountAlert({
+    premium_discount: "-0.01",
+    median_30_day_spread: "0.14",
+    etf_ticker: "EBI",
+    exchange: "NYSE",
+    cusip: "0000000000",
+    gross_expense_ratio: "0.35%",
+    net_assets: "100000000",
+  }).then((data) => {
+    console.log(data);
+  });
+}
